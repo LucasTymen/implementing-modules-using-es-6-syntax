21735abe88730cdbfb9bbb12baba7a04baec08ac
Lucas Tymen <lucas.tymen@gmail.com>
2023-02-27 14:51:23 +0100

    exempleJS-1

--- modules/dom-functions.js
+++ modules/dom-functions.js
@@ -7,6 +7,16 @@ const toggleHiddenElement = (domElement) => {
   }
 }
 
+import { toggleHiddenElement, changeToFunkyColor } from '../modules/dom-functions.js';
+
+const buttonElement = document.getElementById('secret-button');
+const pElement = document.getElementById('secret-p');
+
+buttonElement.addEventListener('click', () => {
+  toggleHiddenElement(pElement);
+  changeToFunkyColor(buttonElement);
+});
+
 const changeToFunkyColor = (domElement) => {
 const r = Math.random() * 255;
 const g = Math.random() * 255;
